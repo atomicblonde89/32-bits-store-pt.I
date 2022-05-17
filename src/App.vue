db9cd8478ef90aed1af7338f9c92d22bd488f91a
Demian <learc47@gmail.com>
2022-05-16 23:42:01 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <h1 class="mx-auto">Tienda 32 Bits</h1>
+    <h3>Lista de juegos</h3>
+    <table cellpadding="0" border="3px">
+      <thead>
+        <th>Código</th>
+        <th>Nombre</th>
+        <th>Stock</th>
+        <th>Precio</th>
+      </thead>
+      <tbody>
+        <tr v-for="(game, i) in games" :key="i" :style="{color: game.color }">
+          <td>{{ game.codigo }} </td> 
+          <td>{{ game.nombre }} </td>
+          <td>{{ game.stock }} </td>
+          <td>${{parseInt(game.precio).toLocaleString()}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import { mapState } from 'vuex'
 
-nav {
-  padding: 30px;
-}
+  export default {
+    name: 'App',
+    computed: {
+      ...mapState(['games']),
+    },
+  }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import games from "@/assets/games.json"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    games: games
   },
   getters: {
   },
